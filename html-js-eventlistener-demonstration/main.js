8c8e3c601cf5301a4d6b3e2f4ab452f48aab2904
Ignacy Łątka <74246391+LatekVo@users.noreply.github.com>
2023-05-24 18:25:21 +0200

    updated event listener toy

--- html-js-eventlistener-demonstration/main.js
+++ html-js-eventlistener-demonstration/main.js
@@ -24,29 +24,59 @@ function addEditable() {
     editableCount++
 }
 
-function fontIncrease(event) {
-    let fontSize = parseInt(event.target.dataset.fontSize)
+function fontIncrease() {
+    let targetElement = document.getElementById(currentTargetId)
+
+    let fontSize = parseInt(targetElement.dataset.fontSize)
+    fontSize += 2
+
+    targetElement.style.fontSize = fontSize + "px"
+    targetElement.dataset.fontSize = fontSize
 }
 
-function fontDecrease(event) {
+function fontDecrease() {
+    let targetElement = document.getElementById(currentTargetId)
 
+    let fontSize = parseInt(targetElement.dataset.fontSize)
+    fontSize -= 2
+
+    targetElement.style.fontSize = fontSize + "px"
+    targetElement.dataset.fontSize = fontSize
 }
 
-function fontColorChange(event) {
+function fontColorChange() {
+    let targetElement = document.getElementById(currentTargetId)
 
+    getRand = () => {
+        return Math.floor(Math.random() * 70);
+    }
+    
+    let newColor = `rgb(${getRand()}, ${getRand()}, ${getRand()})` 
+
+    console.log(newColor)
+
+    targetElement.style.color = newColor
 }
 
-function backgroundColorChange(event) {
+function backgroundColorChange() {
+    let targetElement = document.getElementById(currentTargetId)
+
+    getRand = () => {
+        return 255 - Math.floor(Math.random() * 35);
+    }
+
+    let newColor = `rgb(${getRand()}, ${getRand()}, ${getRand()})` 
+
+    console.log(newColor)
+
+    targetElement.style.backgroundColor = newColor
 
 }
-
-document.querySelector('#sizeUp').addEventListener('click', fontIncrease)
-document.querySelector('#sizeUp').addEventListener('click', fontIncrease)
-document.querySelector('#sizeUp').addEventListener('click', fontIncrease)
-document.querySelector('#sizeUp').addEventListener('click', fontIncrease)
-
 
 function initialize() {
+    
+    // init enviorment
+    
     editArea = document.querySelector('#editArea')
     
     let oldField = document.querySelector('.editElement') // select the first element and duplicate it
@@ -59,4 +89,12 @@ function initialize() {
     oldField.classList.add('target')
     oldField.id = 0
     oldField.addEventListener('click', setFocus)
+
+    // add listeners
+
+    document.querySelector('#sizeUp').addEventListener('click', fontIncrease)
+    document.querySelector('#sizeDown').addEventListener('click', fontDecrease)
+    document.querySelector('#colorRandom').addEventListener('click', fontColorChange)
+    document.querySelector('#backgroundRandom').addEventListener('click', backgroundColorChange)
+    document.querySelector('#editableAdd').addEventListener('click', addEditable)
 }
